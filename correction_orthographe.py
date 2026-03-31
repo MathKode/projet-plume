aed4ecd5db1f9db1a902d66bab3c333876974332
BiMathAx <61063210+MathKode@users.noreply.github.com>
2026-03-31 22:39:10 +0000

    a

--- correction_orthographe.py
+++ correction_orthographe.py
@@ -198,14 +198,14 @@ def correction_orthographe_page():
                         changement_d1 = ""
                         for i in d1.split(' '):
                                 if i not in d2_ls:
-                                    changement_d1 = changement_d1 + f" #{i}# "
+                                    changement_d1 = changement_d1 + f" **{i}** "
                                 else :
                                     changement_d1 = changement_d1 + f" {i} "
                         d1_ls=d1.split(' ')
                         changement_d2 = ""
                         for i in d2.split(' '):
                                 if i not in d1_ls:
-                                    changement_d2 = changement_d2 + f" #{i}# "
+                                    changement_d2 = changement_d2 + f" **{i}** "
                                 else :
                                     changement_d2 = changement_d2 + f" {i} "
                         return changement_d1, changement_d2
@@ -216,8 +216,17 @@ def correction_orthographe_page():
                     idn=0
                     for i in st.session_state.notion_ls:
                         is_supprimee = i in st.session_state.correction_supprimees
+                        
+                        if is_supprimee:
+                                #button_type = "secondary"
+                                emoji = "🗑️"
+                                label = f"{emoji} ~~{modif(i[0],i[1])}~~"
+                            else:
+                                #button_type = "primary"
+                                emoji = "✅"
+                                label = f"{emoji} {modif(i[0],i[1])}"
 
-                        if st.button(f"{modif(i[0],i[1])}",
+                        if st.button(label,
                                    key=f"correction_{idn}",
                                    use_container_width=True
                         ):
